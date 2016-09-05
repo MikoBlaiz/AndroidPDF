051e90236fb01e3c64a2028a72d5b6dc534d1f5b
miko <miko@wide-web.spb.ru>
2016-09-05 10:55:16 +0300

    first commit

---
+++ pdf/src/main/java/com/mikoblaiz/pdf/LinkInfoVisitor.java
@@ -0,0 +1,9 @@
+package com.mikoblaiz.pdf;
+
+abstract public class LinkInfoVisitor {
+	public abstract void visitInternal(LinkInfoInternal li);
+
+	public abstract void visitExternal(LinkInfoExternal li);
+
+	public abstract void visitRemote(LinkInfoRemote li);
+}
---
+++ pdf/src/main/java/com/mikoblaiz/pdf/Separation.java
@@ -0,0 +1,16 @@
+package com.mikoblaiz.pdf;
+
+public class Separation
+{
+	String name;
+	int rgba;
+	int cmyk;
+
+	public Separation(String name, int rgba, int cmyk)
+	{
+		this.name = name;
+		this.rgba = rgba;
+		this.cmyk = cmyk;
+	}
+}
+
